ded9e9d7ddeeb99f1b011cdce3d3669180a547ec
s.fastner <s.fastner@external.telekom.de>
2010-10-05 16:42:44 +0200

    Add HTML5 Class handler if available to unify's qooxdoo fork of class handling in bom

--- qooxdoo/framework/source/class/qx/bom/element2/Class.js
+++ qooxdoo/framework/source/class/qx/bom/element2/Class.js
@@ -1,4 +1,4 @@
-/* ************************************************************************
+﻿/* ************************************************************************
 
    qooxdoo - the new era of web development
 
@@ -14,6 +14,7 @@
 
    Authors:
      * Sebastian Werner (wpbasti)
+	 * Sebastian Fastner (fastner)
 
    ======================================================================
 
@@ -54,20 +55,32 @@ qx.Class.define("qx.bom.element2.Class",
      * @param elem {Element} DOM element to query
      * @param value {String} Class name to query for
      */
-    has : function(elem, value)
-    {
-      if (qx.core.Variant.isSet("qx.debug", "on"))
+    has : qx.lang.Object.select(qx.bom.client.Feature.HTML5_CLASSLIST ? "native" : "default",
+	{
+	  "native" : function(elem, value) {
+	    if (qx.core.Variant.isSet("qx.debug", "on"))
+        {
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          qx.core.Assert.assertString(value, "Invalid class name to check for!");
+          qx.core.Assert.assertMatch(value, /^[a-zA-Z][a-zA-Z0-9-]+$/, "Supports single value class names only!");
+        }
+	    return elem.classList.contains(name);
+	  },
+	  "default" : function(elem, value)
       {
-        qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
-        qx.core.Assert.assertString(value, "Invalid class name to check for!");
-        qx.core.Assert.assertMatch(value, /^[a-zA-Z][a-zA-Z0-9-]+$/, "Supports single value class names only!");
-      }
+        if (qx.core.Variant.isSet("qx.debug", "on"))
+        {
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          qx.core.Assert.assertString(value, "Invalid class name to check for!");
+          qx.core.Assert.assertMatch(value, /^[a-zA-Z][a-zA-Z0-9-]+$/, "Supports single value class names only!");
+        }
 
-      if ((" " + elem.className + " ").replace(this.__rclass, " ").indexOf(" " + value + " ") > -1) {
-        return true;
-      }
+        if ((" " + elem.className + " ").replace(this.__rclass, " ").indexOf(" " + value + " ") > -1) {
+          return true;
+        }
 
-      return false;
+        return false;
+	  }
     },
 
 
@@ -79,35 +92,59 @@ qx.Class.define("qx.bom.element2.Class",
      *    multiple classes in a single string.
      * @return {String} The resulting class name which was applied
      */
-    add : function(elem, classes)
-    {
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
-        qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
-      }
-
-      if (!elem.className)
-      {
-        elem.className = classes;
-      }
-      else
-      {
-        var classNames = classes.split(this.__rspace);
-        var className = " " + elem.className + " ";
-        var setClass = elem.className;
-
-        for (var i=0, l=classNames.length; i<l; i++)
+    add : qx.lang.Object.select(qx.bom.client.Feature.HTML5_CLASSLIST ? "native" : "default",
+	{
+	  "native" : function(elem, classes)
+	  {
+	    if (qx.core.Variant.isSet("qx.debug", "on"))
         {
-          if (className.indexOf(" " + classNames[i] + " ") < 0) {
-            setClass += " " + classNames[i];
-          }
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
+        }
+		
+		if (!elem.className)
+        {
+          elem.className = classes;
+        }
+        else
+        {
+          var classNames = classes.split(this.__rspace);
+		  
+		  for (var i=0, l=classNames.length; i<l; i++) {
+		    element.classList.add(classNames[i]);
+		  }
+		}
+	  },
+	  "default" : function(elem, classes)
+      {
+        if (qx.core.Variant.isSet("qx.debug", "on"))
+        {
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
         }
 
-        elem.className = qx.lang.String.trim(setClass);
-      }
+        if (!elem.className)
+        {
+          elem.className = classes;
+        }
+        else
+        {
+          var classNames = classes.split(this.__rspace);
+          var className = " " + elem.className + " ";
+          var setClass = elem.className;
 
-      return elem.className;
+          for (var i=0, l=classNames.length; i<l; i++)
+          {
+            if (className.indexOf(" " + classNames[i] + " ") < 0) {
+              setClass += " " + classNames[i];
+            }
+          }
+
+          elem.className = qx.lang.String.trim(setClass);
+        }
+
+        return elem.className;
+	  }
     },
 
 
@@ -119,35 +156,65 @@ qx.Class.define("qx.bom.element2.Class",
      *    multiple classes in a single string.
      * @return {String} The resulting class name which was applied
      */
-    remove : function(elem, classes)
-    {
-      if (qx.core.Variant.isSet("qx.debug", "on"))
+    remove : qx.lang.Object.select(qx.bom.client.Feature.HTML5_CLASSLIST ? "native" : "default",
+	{
+	  "native" : function(elem, classes)
+	  {
+	    if (qx.core.Variant.isSet("qx.debug", "on"))
+        {
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          //qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
+        }
+		
+		if (!elem.className) {
+          return;
+        }
+		
+		if (classes)
+        {
+          var classNames = classes.split(this.__rspace);
+
+          for (var i=0, l=classNames.length; i<l; i++) {
+            element.classList.remove(classNames[i]);
+          }
+        }
+        else
+        {
+          elem.className = "";
+        }
+		
+		return elem.className;
+	  },
+	  "default" : function(elem, classes)
       {
-        qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
-        qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
-      }
-
-      if (!elem.className) {
-        return;
-      }
-
-      if (classes)
-      {
-        var classNames = classes.split(this.__rspace);
-        var setClass = (" " + elem.className + " ").replace(this.__rclass, " ");
-
-        for (var i=0, l=classNames.length; i<l; i++) {
-          setClass = setClass.replace(" " + classNames[i] + " ", " ");
+        if (qx.core.Variant.isSet("qx.debug", "on"))
+        {
+          qx.core.Assert.assertElement(elem, "Invalid element to check for applied CSS class!");
+          //qx.core.Assert.assertString(classes, "Invalid class name to add to element!");
         }
 
-        elem.className = qx.lang.String.trim(setClass);
-      }
-      else
-      {
-        elem.className = "";
-      }
+        if (!elem.className) {
+          return;
+        }
 
-      return elem.className;
+        if (classes)
+        {
+          var classNames = classes.split(this.__rspace);
+          var setClass = (" " + elem.className + " ").replace(this.__rclass, " ");
+
+          for (var i=0, l=classNames.length; i<l; i++) {
+            setClass = setClass.replace(" " + classNames[i] + " ", " ");
+          }
+
+          elem.className = qx.lang.String.trim(setClass);
+        }
+        else
+        {
+          elem.className = "";
+        }
+
+        return elem.className;
+      }
     }
   }
 });
